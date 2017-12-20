0fabfc9abc36a5e9efb88d5d5f55eb1ee60ab480
lyfankai@163.com <lyfankai@163.com>
2017-12-20 10:33:12 +0800

    这是一个多聚道打包案例，及使用说明和需要资料都在项目下面

---
+++ AppPackTest/app/src/main/java/com/atguigu/apppacktest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.atguigu.apppacktest;
+
+import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String channel = getChannel();
+        Toast.makeText(MainActivity.this,channel, Toast.LENGTH_SHORT).show();
+    }
+
+    private String getChannel() {
+        try {
+            PackageManager pm = getPackageManager();
+            ApplicationInfo appInfo = pm.getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            return appInfo.metaData.getString("UMENG_CHANNEL");
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return "";
+    }
+}
